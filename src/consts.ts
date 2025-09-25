7f08940e75d9ec63faa76d699b2cfcb76cc1d771
desingkuro <andresmauricio745@gmail.com>
2025-09-24 20:30:59 -0500

    configuracion inicial

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Crispy Cookie';
+export const SITE_DESCRIPTION = 'Welcome to my cookie blog!';
